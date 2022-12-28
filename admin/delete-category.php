382cafaee8fe66038e2d7b6d149144af2c7dc902
Sazzat <asikulislamsazzat@gmail.com>
2022-12-28 19:09:14 +0600

    first commit

---
+++ admin/delete-category.php
@@ -0,0 +1,20 @@
+<?php
+include "config.php";
+if($_SESSION["user_role"]=='0'){
+    header("Location: {$hostname}/admin/post.php");
+  }
+$categoryid=$_GET['id'];
+$sql="DELETE FROM category WHERE category_id={$categoryid}";
+
+
+if(mysqli_query($conn,$sql)){
+    header("Location: {$hostname}/admin/category.php");
+
+}else{
+echo "<p style='color:red;margin:10px 0;'> Can't Delete the user Record</p>";
+
+}
+mysqli_close($conn);
+
+
+?>
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$hostname="http://localhost/learnphp/87.News%20Project";
+$conn=mysqli_connect("localhost","root","","news-site")or die("Connection Failed :" .mysqli_connect_error());
+
+?>
